3b87e58b1174a4adb8fe1dfe0df70f49cecbf24b
vijay <vijay.roark@gmail.com>
2017-03-14 18:54:23 -0700

    adding updated apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,16 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # install apache2 package
+
+if node['platform_family']=="debian"
+package = "apache2"
+elsif node['platform_family']=="rhel"
+package = "httpd"
+end
 package 'httpd' do
-          package_name 'apache2'
+
+    package_name package
+#          package_name 'apache2'
           action :install
 end
 
@@ -13,4 +21,10 @@ service 'httpd' do
        service_name 'apache2'
        action [:start, :enable]
 end
+
+service 'apache' do
+service_name 'httpd'
+action [:start, :enable]
+end
+
 #include_recipe 'apache::websites'

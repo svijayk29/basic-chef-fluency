8c3e096a281c4facaff141eb45a0eacd1b175065
vijay <vijay.roark@gmail.com>
2017-03-15 19:04:44 -0700

    fixed service startup issues

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -20,14 +20,28 @@ end
 #service 'web' do
 if node['platform_family']=="debian"
 
-    service_name = "apache2"
-  #    service = 'apache2'
-      action [:start, :enable]
+service 'apache' do
+service_name 'apache2'
+action [:start, :enable]
+end
+
 elsif node['platform_family']=="rhel"
-service_name = "httpd"
-     # service = 'apache2'
-      action [:start, :enable]
-    end
+service 'httpd' do
+service_name 'httpd'
+action [:start, :enable]
+end
+
+end
+
+
+#    service_name = "apache2"
+   #  service = 'apache2'
+    #  action [:start, :enable]
+#elsif node['platform_family']=="rhel"
+#service_name = "httpd"
+ #     service = 'httpd'
+  #    action [:start, :enable]
+   # end
 
 
    #elsif service_name == "httpd"
